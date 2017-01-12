478f3225c56b87decb62188c993bddee105a956d
davygora <davygora.e@gmail.com>
2017-01-12 21:31:35 +0200

    init commit

---
+++ bot.rb
@@ -0,0 +1,12 @@
+require 'telegram/bot'
+
+token = 'TOKEN'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.sendMessage(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    end
+  end
+end
